e4cffb5db81649f3a904d22668c6014796a6694c
Sechaba Motaung <sechabamot@gmail.com>
2021-12-18 08:45:36 +0200

    Add project files.

---
+++ Controllers/TaxRateController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Charity_Calculator_Challange.Controllers
+{
+    [ApiController]
+    public class TaxRateController : ControllerBase
+    {
+        public TaxRateController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();      
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+    }
+}
